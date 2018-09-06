336dfd945b6ce2b73cd8055e603a251af63a533d
StognienkoVladislav <stognienkovv@gmail.com>
2018-09-06 13:23:09 +0300

    DB + logging

--- InteractionsWithAPI/app.py
+++ InteractionsWithAPI/app.py
@@ -1,17 +1,29 @@
 import json
+import hashlib
+import logging
+import datetime
+import requests
 
 from create_db import Payments
 from forms import PaymentForm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask import Flask, redirect, url_for, render_template, flash, request, session
-import requests
 
 
 app = Flask(__name__)
 app.secret_key = "SecretKey01"
 shop_id = 5
+shop_order_id = 123456
 payway = "payeer_rub"
+
+logger = logging.getLogger('Loger')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('info.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 
 @app.route('/')
@@ -28,19 +40,6 @@ def payments():
         currency = form.payment_currency.data
 
         currencies = {'USD': 'usd_api', 'EUR': 'eur_api', 'RUB': 'rub_api'}
-
-        """
-        engine = create_engine('sqlite:///payments.db', echo=True)
-        # create a Session
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        payment = Payments(request.form['payment_amount'], request.form['payment_currency'], request.form['description'])
-        session.add(payment)
-
-        # commit the record database
-        session.commit()
-        """
         print(request.form['payment_currency'], request.form['description'], '!!!')
         values = [request.form['payment_amount'],
                   request.form['payment_currency'],
@@ -51,9 +50,39 @@ def payments():
     return render_template('payments.html', title='Payments', form=form)
 
 
+def add_to_DB(*args):
+    engine = create_engine('sqlite:///payments.db', echo=True)
+    # create a Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    payment = Payments(args[0], args[1], args[2], args[3])
+    session.add(payment)
+
+    # commit the record database
+    session.commit()
+
+
+def create_sign(*args):
+    print(args)
+    str_for_hash = ":".join(str(arg) for arg in args)
+    str_for_hash += app.secret_key
+    print(str_for_hash)
+    hashh = hashlib.sha256(str.encode(str_for_hash)).hexdigest()
+    return hashh
+
+
 @app.route('/eur_api', methods=['POST', 'GET'])
 def eur_api():
+    titles = ['amount', 'currency', 'description']
+    dictt = {}
+    for val, x in zip(titles, session.pop("values", [])):
+        dictt[val] = x
 
+    sign = create_sign(':'.join(str(x) for x in [dictt['amount'], dictt['currency'], shop_id, shop_order_id]))
+    print(sign)
+    add_to_DB(float(dictt['amount']), dictt['currency'], dictt['description'], datetime.datetime.now())
+    logger.info('Create EUR pay with values:'+', '.join([dictt['amount'], dictt['currency'], dictt['description']]))
     return render_template('eur_test_templ.html')
 
 
@@ -95,6 +124,17 @@ def usd_api():
     data = r.json()
     link = data['data']['url']
     return redirect(link)
+
+
+@app.route('/test_db')
+def test_db():
+    engine = create_engine('sqlite:///payments.db', echo=True)
+    # create a Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    payments = session.query(Payments).all()
+    return ','.join(payment.description for payment in payments)
 
 
 if __name__ == '__main__':
--- InteractionsWithAPI/create_db.py
+++ InteractionsWithAPI/create_db.py
@@ -9,17 +9,19 @@ Base = declarative_base()
 
 class Payments(Base):
     """"""
-    __tablename__ = 'notes'
+    __tablename__ = 'payments'
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     payment_amount = Column(Float)
     payment_currency = Column(String)
     description = Column(Text)
+    time = Column(DateTime)
 
-    def __init__(self, payment_amount, payment_currency, description):
+    def __init__(self, payment_amount, payment_currency, description, time):
 
         self.payment_amount = payment_amount
         self.payment_currency = payment_currency
         self.description = description
+        self.time = time
 
 
 Base.metadata.create_all(engine)
