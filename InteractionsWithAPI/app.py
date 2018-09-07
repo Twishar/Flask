39e762e1fa5ae59a4d4d92df419c02806248ed03
StognienkoVladislav <stognienkovv@gmail.com>
2018-09-07 21:11:59 +0300

    review

--- InteractionsWithAPI/app.py
+++ InteractionsWithAPI/app.py
@@ -4,20 +4,20 @@ import logging
 import datetime
 import requests
 
+from config import *
 from create_db import Payments
 from forms import PaymentForm
 from sqlalchemy import create_engine
 from flask_bootstrap import Bootstrap
 from sqlalchemy.orm import sessionmaker
 from flask import Flask, redirect, url_for, render_template, flash, request, session
+from flask_sslify import SSLify
 
 app = Flask(__name__)
-app.secret_key = "SecretKey01"
-bootstrap = Bootstrap(app)
+app.secret_key = secret_key
 
-shop_id = 5
-shop_order_id = 123456
-payway = "payeer_rub"
+bootstrap = Bootstrap(app)
+SSLify(app)
 
 logger = logging.getLogger('Loger')
 logger.setLevel(logging.DEBUG)
@@ -28,31 +28,104 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-@app.route('/')
-def index():
-    return "GOOD"
-
-
-@app.route('/payments', methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def payments():
 
     form = PaymentForm()
     if form.validate_on_submit():
 
         currency = form.payment_currency.data
-        currency_code = {'USD': '840', 'EUR': '978', 'RUB': '643'}
         currencies = {'USD': 'usd_api', 'EUR': 'eur_api', 'RUB': 'rub_api'}
-        print(request.form['payment_currency'], request.form['description'], '!!!')
+
         values = [request.form['payment_amount'],
                   currency_code[request.form['payment_currency']],
                   request.form['description']]
+
         session['values'] = values
         return redirect(url_for('{}'.format(currencies[currency])))
 
-    return render_template('payments.html', title='Payments', form=form)
+    return render_template('payments.html', form=form)
 
 
-def add_to_DB(*args):
+@app.route('/eur_api', methods=['POST', 'GET'])
+def eur_api():
+
+    data_from_form = get_data_from_session(session)
+
+    sign = create_sign(':'.join(str(x) for x in [data_from_form['amount'], data_from_form['currency'],
+                                                 shop_id, shop_order_id]))
+
+    add_to_db(float(data_from_form['amount']), data_from_form['currency'],
+              data_from_form['description'], datetime.datetime.now())
+
+    logger.info('Create EUR pay with amount: {} and description: {}'.format(data_from_form['amount'],
+                                                                            data_from_form['description']))
+
+    data_from_form['sign'] = sign
+    data_from_form['shop_id'] = shop_id
+    data_from_form['shop_order_id'] = shop_order_id
+    return render_template('api_for_eur.html', dictt=data_from_form)
+
+
+@app.route('/rub_api')
+def rub_api():
+
+    data_from_form = get_data_from_session(session)
+
+    data_for_request = {
+            "amount": data_from_form['amount'],
+            "currency": data_from_form['currency'],
+            "payway": payway,
+            "shop_id": shop_id,
+            "shop_order_id": shop_order_id,
+            "sign": create_sign(data_from_form['amount'], data_from_form['currency'], payway, shop_id, shop_order_id)
+            }
+
+    r = requests.post('https://core.piastrix.com/invoice/create', data=json.dumps(data_for_request),
+                      headers={'Content-Type': 'application/json'})
+
+    data_for_api = r.json()
+
+    add_to_db(float(data_from_form['amount']), data_from_form['currency'],
+              data_from_form['description'], datetime.datetime.now())
+
+    logger.info('Create RUB pay with amount: {} and description: {}'.format(data_from_form['amount'],
+                                                                            data_from_form['description']))
+
+    return render_template('api_for_rub.html', data=data_for_api['data']['data'],
+                           url=data_for_api['data']['url'], method=data_for_api['data']['method'])
+
+
+@app.route('/usd_api', methods=['POST', 'GET'])
+def usd_api():
+
+    data_from_form = get_data_from_session(session)
+
+    data_for_request = {
+            "payer_currency": data_from_form['currency'],
+            "shop_amount": data_from_form['amount'],
+            "shop_currency": data_from_form['currency'],
+            "shop_id": shop_id,
+            "shop_order_id": shop_order_id,
+            "sign": create_sign(data_from_form['currency'], data_from_form['amount'],
+                                data_from_form['currency'], shop_id, shop_order_id)
+            }
+
+    r = requests.post('https://core.piastrix.com/bill/create', data=json.dumps(data_for_request),
+                      headers={'Content-Type': 'application/json'})
+
+    add_to_db(float(data_from_form['amount']), data_from_form['currency'],
+              data_from_form['description'], datetime.datetime.now())
+
+    logger.info('Create USD pay with amount: {} and description: {}'.format(data_from_form['amount'],
+                                                                            data_from_form['description']))
+
+    data_for_api = r.json()
+    link = data_for_api['data']['url']
+    return redirect(link)
+
+
+def add_to_db(*args):
     engine = create_engine('sqlite:///payments.db', echo=True)
     # create a Session
     Session = sessionmaker(bind=engine)
@@ -66,97 +139,18 @@ def add_to_DB(*args):
 
 
 def create_sign(*args):
-    print(args)
-    str_for_hash = ":".join(str(arg) for arg in args)
-    str_for_hash += app.secret_key
-    print(str_for_hash)
+    str_for_hash = ":".join(str(arg) for arg in args) + app.secret_key
     hashh = hashlib.sha256(str.encode(str_for_hash)).hexdigest()
     return hashh
 
 
-@app.route('/eur_api', methods=['POST', 'GET'])
-def eur_api():
+def get_data_from_session(session_):
     titles = ['amount', 'currency', 'description']
-    dictt = {}
-    for val, x in zip(titles, session.pop("values", [])):
-        dictt[val] = x
+    data_for_api = {}
 
-    sign = create_sign(':'.join(str(x) for x in [dictt['amount'], dictt['currency'], shop_id, shop_order_id]))
-
-    add_to_DB(float(dictt['amount']), dictt['currency'], dictt['description'], datetime.datetime.now())
-    logger.info('Create EUR pay with amount: {} and description: {}'.format(dictt['amount'], dictt['description']))
-    dictt['sign'] = sign
-    dictt['shop_id'] = shop_id
-    dictt['shop_order_id'] = shop_order_id
-    return render_template('eur_test_templ.html', dictt=dictt)
-
-
-@app.route('/rub_api')
-def rub_api():
-
-    titles = ['amount', 'currency', 'description']
-    dictt = {}
-    for val, x in zip(titles, session.pop("values", [])):
-        dictt[val] = x
-
-    data = {
-            "amount": dictt['amount'],
-            "currency": dictt['currency'],
-            "payway": payway,
-            "shop_id": shop_id,
-            "shop_order_id": shop_order_id,
-            "sign": create_sign(dictt['amount'], dictt['currency'], payway, shop_id, shop_order_id)
-            }
-
-    r = requests.post('https://core.piastrix.com/invoice/create', data=json.dumps(data),
-                      headers={'Content-Type': 'application/json'})
-    print(r.headers['content-type'])
-    print(r.json())
-    data = r.json()
-
-    add_to_DB(float(dictt['amount']), dictt['currency'], dictt['description'], datetime.datetime.now())
-    logger.info('Create RUB pay with amount: {} and description: {}'.format(dictt['amount'], dictt['description']))
-
-    return render_template('rub_templ.html', data=data['data']['data'])
-
-
-@app.route('/usd_api', methods=['POST', 'GET'])
-def usd_api():
-    payer_currency = '840'
-    titles = ['amount', 'currency', 'description']
-    dictt = {}
-    for val, x in zip(titles, session.pop("values", [])):
-        dictt[val] = x
-
-    data = {"payer_currency": payer_currency,
-            "shop_amount": dictt['amount'],
-            "shop_currency": dictt['currency'],
-            "shop_id": shop_id,
-            "shop_order_id": shop_order_id,
-            "sign": create_sign(payer_currency, dictt['amount'], dictt['currency'], shop_id, shop_order_id)
-            }
-
-    r = requests.post('https://core.piastrix.com/bill/create', data=json.dumps(data), headers={'Content-Type': 'application/json'})
-    print(r.headers['content-type'])
-    print(r.json())
-
-    add_to_DB(float(dictt['amount']), dictt['currency'], dictt['description'], datetime.datetime.now())
-    logger.info('Create USD pay with amount: {} and description: {}'.format(dictt['amount'], dictt['description']))
-
-    data = r.json()
-    link = data['data']['url']
-    return redirect(link)
-
-
-@app.route('/test_db')
-def test_db():
-    engine = create_engine('sqlite:///payments.db', echo=True)
-    # create a Session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    payments = session.query(Payments).all()
-    return ','.join(payment.description for payment in payments)
+    for title, value in zip(titles, session_.pop("values", [])):
+        data_for_api[title] = value
+    return data_for_api
 
 
 if __name__ == '__main__':
---
+++ InteractionsWithAPI/config.py
@@ -0,0 +1,6 @@
+
+shop_id = 5
+shop_order_id = 123456
+payway = "payeer_rub"
+secret_key = "SecretKey01"
+currency_code = {'USD': '840', 'EUR': '978', 'RUB': '643'}
