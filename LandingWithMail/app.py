97fb25b30be5f37fbb0d28377afc4c233daf610a
StognienkoVladyslav <stognienkovv@gmail.com>
2018-09-23 14:19:45 +0300

    gmail serv

--- LandingWithMail/app.py
+++ LandingWithMail/app.py
@@ -1,8 +1,18 @@
 
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'Your_mail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Your_mail_password'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+
+mail = Mail(app)
 
 
 @app.route('/')
@@ -15,8 +25,19 @@ def index():
 def guest():
 
     name = request.form.get('name')
-    print(name)
-    return '<h1>ASFASF</h1>'
+    email = request.form.get('email')
+    link = request.form.get('link')
+    description = request.form.get('description')
+    msg = Message('Info from Guest form', sender='Your_mail@gmail.com',
+                  recipients=['recipients@gmail.com'])
+    msg.body = "User name: {}\n" \
+               "User email: {}\n" \
+               "Guest link: {}\n" \
+               "Why he?: {}\n".format(name, email,
+                                      link, description)
+    with app.app_context():
+        mail.send(msg)
+    return redirect(url_for('index'))
 
 
 app.run()
