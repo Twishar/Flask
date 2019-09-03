7cec2c74242e97b31da078be1e21b01a3de2ee5c
StognienkoVladislav <stognienkovv@gmail.com>
2019-09-03 14:22:47 +0300

    added handlers and marshmallow schemas

--- Advance/section2/app.py
+++ Advance/section2/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
 from blacklist import BLACKLIST
+from marshmallow_templ import ma
+from marshmallow import ValidationError
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
 
 app = Flask(__name__)
@@ -24,6 +28,11 @@ def create_tables():
     db.create_all()
 
 
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):  # except ValidationError as err
+    return jsonify(err.messages), 400
+
+
 jwt = JWTManager(app)
 
 
@@ -33,6 +42,10 @@ def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
 
 
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
@@ -41,4 +54,5 @@ api.add_resource(UserLogout, "/logout")
 
 if __name__ == "__main__":
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
--- Advance/section2/resources/user.py
+++ Advance/section2/resources/user.py
@@ -28,15 +28,11 @@ user_schema = UserSchema()
 class UserRegister(Resource):
     @classmethod
     def post(cls):
-        try:
-            user_data = user_schema.load(request.get_json())
-        except ValidationError as err:
-            return err.messages, 400
+        user = user_schema.load(request.get_json())
 
-        if UserModel.find_by_username(user_data["username"]):
+        if UserModel.find_by_username(user.username):
             return {"message": USER_ALREADY_EXISTS}, 400
 
-        user = UserModel(**user_data)
         user.save_to_db()
 
         return {"message": CREATED_SUCCESSFULLY}, 201
@@ -67,15 +63,12 @@ class User(Resource):
 class UserLogin(Resource):
     @classmethod
     def post(cls):
-        try:
-            data = user_schema.load(request.get_json())
-        except ValidationError as err:
-            return err.messages, 400
 
-        user = UserModel.find_by_username(data["username"])
+        user_data = user_schema.load(request.get_json())
+        user = UserModel.find_by_username(user_data.username)
 
         # this is what the `authenticate()` function did in security.py
-        if user and safe_str_cmp(user.password, data["password"]):
+        if user and safe_str_cmp(user.password, user_data.password):
             # identity= is what the identity() function did in security.py—now stored in the JWT
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
