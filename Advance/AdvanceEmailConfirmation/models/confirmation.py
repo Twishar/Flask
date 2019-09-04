2a829f63011c134b4bed25a9801884864147ad31
StognienkoVladislav <stognienkovv@gmail.com>
2019-09-04 19:58:29 +0300

    added confirmation

---
+++ Advance/AdvanceEmailConfirmation/models/confirmation.py
@@ -0,0 +1,30 @@
+
+from db import db
+from time import time
+from uuid import uuid4
+
+CONFIRMATION_EXPIRATION_DELTA = 1800  # 30 minutes
+
+
+class ConfirmationModel(db.Model):
+    __tablename__ = "confirmations"
+
+    id = db.Column(db.String(50), primary_key=True)
+    expire_at = db.Column(db.Integer, nullable=False)
+    confirmed = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("UserModel")
+
+    def __init__(self, user_id: int, **kwargs):
+        super().__init__(**kwargs)
+        self.user_id = user_id
+        self.id = uuid4().hex
+        self.expire_at = int(time()) + CONFIRMATION_EXPIRATION_DELTA
+        self.confirmed = False
+
+    @classmethod
+    def find_by_id(cls, _id: str) -> "ConfirmationModel":
+        return cls.query.filter_by(id=_id).first()
+
+    def expired(self) -> bool:
+        return time() > self.expire_at  # current time > time 30 min ago + confirmation delta
